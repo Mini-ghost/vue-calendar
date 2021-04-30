be760be297d0fbdf68cd03050763a76d8691d14d
Alex Liu <dsa1314@gmail.com>
2021-04-30 10:24:42 +0800

    chore: add demo

--- src/Demo.vue
+++ src/Demo.vue
@@ -1,13 +1,29 @@
 <template>
   <div>
-    <!--  -->
+    <Calendar 
+      :value="new Date(year, month)"
+      :show-adjacent-months="showAdjacentMonths"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
+// Components
+import Calendar from '@/components/Calendar/Calendar'
+
 export default Vue.extend({
-  name: 'App'
+  name: 'App',
+  components: {
+    Calendar
+  },
+  data() {
+    return {
+      year: 2021,
+      month: 3,
+      showAdjacentMonths: false
+    }
+  }
 })
 </script>
