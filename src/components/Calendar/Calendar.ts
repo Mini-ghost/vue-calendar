4ca0dcc9d0b0bde82783b34856e76460f7b1e436
Alex Liu <dsa1314@gmail.com>
2021-04-30 10:23:51 +0800

    feat: add basic calendar component

---
+++ src/components/Calendar/Calendar.ts
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+
+// Types
+import type { VNode } from 'vue'
+import type { PropValidator } from 'vue/types/options'
+
+// Components
+import CalendarTable from './CalendarTable'
+
+export default Vue.extend({
+  name: 'Calendar',
+  props: {
+    showAdjacentMonths: {
+      type: Boolean,
+      default: false
+    },
+    value: {
+      type: Date,
+      default: () => new Date()
+    } as PropValidator<Date>
+  },
+  methods: {
+    genCalendarHead (): VNode {
+      return this.$createElement()
+    },
+    genCalendarBody (): VNode {
+      return this.$createElement(CalendarTable, {
+        props: {
+          showAdjacentMonths: this.showAdjacentMonths,
+          value: this.value
+        }
+      })
+    }
+  },
+  render (): VNode {
+    return this.$createElement('div', [
+      this.genCalendarHead(),
+      this.genCalendarBody()
+    ])
+  }
+})
---
+++ src/components/Calendar/CalendarTable.ts
@@ -0,0 +1,133 @@
+import Vue from 'vue'
+
+// Types
+import type { 
+  VNode,
+  VNodeChildren
+} from 'vue'
+import type { PropValidator } from 'vue/types/options'
+
+const fill = (
+  string: number | string, 
+  length: number = 2, 
+  fillText: string = '0'
+): string => {
+  let stringLength: number
+
+  string = string + ''
+  fillText = fillText + ''
+
+  if ((stringLength = string.split('').length) > length) {
+    return string
+  }
+
+  const difference = length - stringLength
+  fillText = fillText.repeat(difference)
+
+  return fillText + string
+}
+
+const { format } =  new Intl.DateTimeFormat('en-US', { 
+  day: 'numeric', 
+  timeZone: 'UTC' 
+})
+const formatter = (date: string) => format(new Date(date))
+
+export default Vue.extend({
+  name: 'CalendarTable',
+
+  props: {
+    showAdjacentMonths: {
+      type: Boolean,
+      default: false
+    },
+    value: {
+      type: Date,
+      default: () => new Date()
+    } as PropValidator<Date>
+  },
+
+  computed: {
+    valueYear() {
+      return this.value.getFullYear()
+    },
+    valueMonth() {
+      return this.value.getMonth()
+    }
+  },
+
+  methods: {
+    genHead (): VNode {
+      const row = this.genTr(['S', 'M', 'T', 'W', 'T', 'F', 'S'].map(day => {
+        return this.$createElement('th', [day])
+      }))
+      return this.$createElement('thead', row)
+    },
+    genBody (): VNode {
+      const children: VNodeChildren = []
+      let row: VNodeChildren = []
+      let day = new Date(this.valueYear, this.valueMonth, 1).getDay()
+  
+      // 產生上個月會出現在該月份日曆上的部分
+      const prevMonthYear = this.valueMonth ? this.valueYear : this.valueYear - 1
+      const prevMonth = (this.valueMonth + 11) % 12
+
+      const lastDateOfPreviousMonth = new Date(
+        this.valueYear, 
+        this.valueMonth, 0
+      ).getDate()
+
+      while(day--) {
+        const date = `${prevMonthYear}-${fill(prevMonth + 1)}-${lastDateOfPreviousMonth - day}`
+        row.push(this.$createElement('td', this.showAdjacentMonths 
+          ? [formatter(date)] 
+          : []
+        ))
+      }
+
+      // 產生這個月的日期
+      for(let i = 1; i < 31; i++) {
+        const date = `${this.valueYear}-${fill(this.valueMonth)}-${fill(i)}`
+
+        row.push(this.$createElement('td', [formatter(date)]))
+
+        if(row.length === 7) {
+          children.push(this.genTr(row))
+          row = []
+        }
+      }
+
+      // 產生下個月會出現在該月份日曆上的部分
+      const nextMonthYear = this.valueMonth === 11 
+        ? this.valueYear + 1 
+        : this.valueYear
+
+      const nextMonth = (this.valueMonth + 1) % 12
+      let nextMonthDay = 1
+
+      while (row.length < 7) {
+        const date = `${nextMonthYear}-${fill(nextMonth + 1)}-${fill(nextMonthDay++)}`
+        row.push(this.$createElement('td', this.showAdjacentMonths 
+          ? [formatter(date)] 
+          : []
+        ))
+      }
+
+      children.push(this.genTr(row))
+
+      return this.$createElement('tbody', children)
+    },
+    genTr (children: VNodeChildren): VNode[] {
+      return [this.$createElement('tr', [children])]
+    }
+  },
+
+  render (): VNode {
+    return this.$createElement('table', {
+
+    }, [
+      this.genHead(),
+      this.genBody()
+    ])
+  }
+})
