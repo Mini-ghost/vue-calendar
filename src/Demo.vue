7566da61e655ddcecdd1e005d5557c7b2f972dde
Alex Liu <dsa1314@gmail.com>
2021-05-06 19:15:22 +0800

    chore: update demo

--- src/Demo.vue
+++ src/Demo.vue
@@ -1,10 +1,42 @@
 <template>
-  <div>
-    <p>{{ value }}</p>
-    <Calendar 
+  <div class="p-2">
+    <!-- Dashboard -->
+    <div class="flex border border-gary-300 p-3 rounded-lg">
+      <div class="p-2">
+        <label class="pr-2">showAdjacentMonths</label>
+        <input 
+          v-model="showAdjacentMonths"
+          type="checkbox"
+        >
+      </div>
+
+      <div class="p-2">
+        <label class="pr-2">updataByScroll</label>
+        <input 
+          v-model="updataByScroll"
+          type="checkbox"
+        >
+      </div>
+
+      <div class="p-2">
+        <label class="pr-2">selectMode</label>
+        <select v-model="selectMode">
+          <option
+            v-for="option in modeOptions"
+            :key="option"
+            :value="option"
+          >
+            {{ option }}
+          </option>
+        </select>
+      </div>
+    </div>
+
+    <calendar 
       v-model="value"
-      show-adjacent-months
-      update-by-scroll
+      :show-adjacent-months="showAdjacentMonths"
+      :update-by-scroll="updataByScroll"
+      :select-mode="selectMode"
     />
   </div>
 </template>
@@ -22,7 +54,11 @@ export default Vue.extend({
   },
   data() {
     return {
-      value: '2021-05-01'
+      value: '2021-05-01',
+      showAdjacentMonths: false,
+      updataByScroll: false,
+      selectMode: 'single',
+      modeOptions: ['single', 'multiple', 'range']
     }
   }
 })
