7dd300032a28f9b85cb83ad7a7d8e136553d4ca1
Alex Liu <dsa1314@gmail.com>
2021-05-10 11:31:25 +0800

    chore: add jest config that testMatch

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+
+  testMatch: [
+    '**/test/**/*.js',
+    '**/__tests__/**/*.spec.js',
+    '**/__tests__/**/*.spec.ts',
+  ],
 }
